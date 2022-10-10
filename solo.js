8b76694268944ebdf266ac2c8ae3c90fa72e3216
Mahim-pa <115467037+Mahim-pa@users.noreply.github.com>
2022-10-10 17:01:51 +0530

    css,html,js

---
+++ solo.js
@@ -0,0 +1,30 @@
+let home = document.getElementById("para1")
+let homec = 0
+function add1(){
+    homec+=1
+    home.textContent = homec
+}
+function add2(){
+    homec+=2
+    home.textContent = homec
+}
+function add3(){
+    homec+=3
+    home.textContent = homec
+}
+
+
+let guest = document.getElementById("para")
+let guestc = 0
+function add1g(){
+    guestc+=1
+    guest.textContent = guestc
+}
+function add2g(){
+    guestc+=2
+    guest.textContent = guestc
+}
+function add3g(){
+    guestc+=3
+    guest.textContent = guestc
+}
